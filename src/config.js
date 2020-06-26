05659cb72a58cce3ef56d404d6facdd6775dfb48
Kendall Cole <kcole16@gmail.com>
2020-06-25 17:34:40 -0700

    Update config

--- src/config.js
+++ src/config.js
@@ -6,11 +6,11 @@ function getConfig(env) {
     case 'production':
     case 'mainnet':
         return {
-            networkId: 'mainnet',
-            nodeUrl: 'https://rpc.mainnet.near.org',
+            networkId: 'default',
+            nodeUrl: 'https://rpc.testnet.near.org',
             contractName: CONTRACT_NAME,
-            walletUrl: 'https://wallet.mainnet.near.org',
-            helperUrl: 'https://helper.mainnet.near.org',
+            walletUrl: 'https://wallet.testnet.near.org',
+            helperUrl: 'https://helper.testnet.near.org',
         };
     case 'development':
     case 'testnet':
